cd08806f6910a8d5a43bfb8e10ccbf9e3b69e61b
cristobal.poblete <cristobal.poblete@trebol-it.cl>
2021-04-27 00:51:25 -0400

    Login button

--- src/screens/navigations/MainNavigator.js
+++ src/screens/navigations/MainNavigator.js
@@ -3,14 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomePageScreen from '../screens/HomePage/HomePageScreen';
 import LoginScreen from '../screens/Login/Login';
+import SignUpScreen from '../screens/SignUp/SignsUpScreen';
 
 const MainStack = createStackNavigator();
 
-const MainStackScreen = () => {
-  <MainStack.Navigator>
-    <MainStack.Screen name="LogInScreen" component={{LoginScreen}} />
-    <MainStack.Screen name="HomePageScreen" component={{HomePageScreen}} />
-  </MainStack.Navigator>;
-};
+const MainStackScreen = () => (
+  <MainStack.Navigator initialRouteName="LogInScreen">
+    <MainStack.Screen name="LogInScreen" component={LoginScreen} />
+    <MainStack.Screen name="SignUpScreen" component={SignUpScreen} />
+    <MainStack.Screen name="HomePageScreen" component={HomePageScreen} />
+  </MainStack.Navigator>
+);
 
 export default MainStackScreen;
--- src/screens/screens/Login/Login.js
+++ src/screens/screens/Login/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-const LoginScreen = () => {
+const LoginScreen = ({navigation}) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Login Screen</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Login Screen</Text>
+      </View>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Button
+          title="Log in"
+          onPress={() => navigation.navigate('HomePageScreen')}
+        />
+      </View>
     </View>
   );
 };
