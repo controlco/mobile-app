107f7d22041efdb0701f7afd0a16e06347dc2346
Macarena Concha <maconcha4@uc.cl>
2021-04-27 17:04:06 -0400

    login screen

--- src/screens/screens/Login/Login.js
+++ src/screens/screens/Login/Login.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Alert} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 const LoginScreen = ({navigation}) => {
+  const [email, onChangeEmail] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
+
+  // Agregar validaciones de correo con regex.
+  const onLogin = () => {
+    if (email.length == 0 || password.length == 0){
+      Alert.alert('Error', 'Email o constraseña no válidos');
+    } else {
+      navigation.navigate('HomePageScreen')
+    };
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex: 1, alignItems: 'center'}}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Login Screen</Text>
+        <Text>Login</Text>
       </View>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <TextInput
+          style={{ borderWidth: 1, height: 40, margin: 12, width: 150, padding: 10}}
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder={"Correo"}
+        />
+        <TextInput
+          style={{borderWidth: 1, height: 40, margin: 12, width: 150, padding: 10}}
+          onChange={onChangePassword}
+          value={password}
+          placeholder={"Contraseña"}
+          secureTextEntry={true}
+          
+        />
         <Button
           title="Log in"
-          onPress={() => navigation.navigate('HomePageScreen')}
+          onPress={() => onLogin()}
         />
       </View>
     </View>
