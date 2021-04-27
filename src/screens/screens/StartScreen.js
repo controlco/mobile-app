eed9564c2bb0b3ec32aa9ae2c6181e1c7b1906ef
cristobal.poblete <cristobal.poblete@trebol-it.cl>
2021-04-26 22:50:40 -0400

    init project

---
+++ src/screens/screens/StartScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const StartScreen = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default StartScreen;
